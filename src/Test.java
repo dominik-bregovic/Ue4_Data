fbec20a1f15d65d75a68ee072c389fa9400e9a70
dominik-github1 <80465736+dominik-github1@users.noreply.github.com>
2021-06-17 16:07:03 +0200

    last testing

--- src/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Anna".compareToIgnoreCase("Ann"));
-
-        //my_sort.sortByAgeAscending(students,students[0].getAge(),students[students.length-1].getAge());
-    }
-}
